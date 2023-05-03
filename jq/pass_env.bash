737c53d90b6ca6c159448795acb6d9bc96195acc
Bawornsit.C <puksigon@gmail.com>
2023-05-03 15:45:45 +0700

    update: jq pass env

--- jq/pass_env.bash
+++ jq/pass_env.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# This is better way
 SVC_NAME=wallet jq '.services | with_entries(select(.key == env.SVC_NAME)) | .[].short ' ex_data/services.json
+
+echo "===================="
+
+# beware about syntax if you use this method
+SVC_NAME=wallet
+jq ".services | with_entries(select(.key == \"$SVC_NAME\")) | .[].short" ex_data/services.json
