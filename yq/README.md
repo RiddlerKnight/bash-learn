27c002cff7c546e31a3464982faececa093f4cce
Bawornsit.C <puksigon@gmail.com>
2023-05-03 16:00:20 +0700

    doc: yq update: ex_data in yq section

--- yq/README.md
+++ yq/README.md
@@ -3,3 +3,17 @@
 a lightweight and portable command-line YAML processor. yq uses jq like syntax but works with yaml files as well as json. It doesn't yet support everything jq does - but it does support the most common operations and functions, and more is being added continuously.
 
 ref: <https://mikefarah.gitbook.io/yq/>
+
+## YAML Name convention
+
+Kubernetes using camelCase: <https://kubernetes.io/docs/user-guide/jobs/>
+
+`apiVersion` `restartPolicy`
+
+CircleCI using snake_case: <https://circleci.com/docs/1.0/configuration/>
+
+`working_directory` `restore_cache` `store_artifacts`
+
+Jenkins with dash-case: <https://github.com/jenkinsci/yaml-project-plugin/blob/master/samples/google-cloud-storage/.jenkins.yaml>
+
+`stapler-class`
---
+++ yq/select.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "============ 1 =============="
+jq '.services' ex_data/services.json
+echo "============ 2 =============="
+jq '.services.wallet.short' ex_data/services.json
+echo "============ 3 =============="
+jq '.services 
+    | with_entries(select(.key | match("wa*")))' \
+    ex_data/services.json
+echo "============ 4 =============="
+jq '.services 
+    | with_entries(select(.key == "wallet")) 
+    | .[].short' \
+    ex_data/services.json
+echo "============ 5 =============="
+jq '.services 
+    | with_entries(select(.key 
+    | match("wa*"))) 
+    | .[].short' \
+    ex_data/services.json
+echo "============ 6 =============="
+jq '.services 
+    | with_entries(select(.key | match("co*"))) 
+    | .[].short' \
+     ex_data/services.json
