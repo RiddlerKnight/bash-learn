f0451a3018bafa4f80a7617b419c28e4e0e65b19
Bawornsit.C <puksigon@gmail.com>
2023-05-30 14:03:00 +0700

    fix: move back_arrow_with_variables.bash to file ops

--- 8_file_ops/back_arrow_with_variable.bash
+++ 8_file_ops/back_arrow_with_variable.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# WC = Word cout / line count
 wc -l < "README.md"
 
+# <<, >> is known as here-document structure. 
 cat <<EOF
 Hello World
 EOF
@@ -24,6 +26,7 @@ EOF
 
 # Redirect คำสั่งที่จะไปอ่านจาก file 
 # แต่ว่าเราต้องการให้มันอาจจาก ค่าบน terminal แทน อันนี้ใช้แค่ 1 บรรทัด
+# <<< is known as here-string. Instead of typing in text.
 cat <<< fdsafdsa > readme2.txt
 
 echo "======================"
