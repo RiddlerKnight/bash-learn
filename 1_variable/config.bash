e40018bc78819bda41b173b34ce2650524b6bd92
Bawornsit.C <puksigon@gmail.com>
2023-06-27 16:40:29 +0700

    feat: add more examples of variable creation.

--- 1_variable/config.bash
+++ 1_variable/config.bash
@@ -8,7 +8,14 @@ zee=2
 echo "($foo + $baz)" # (I'm foo + 1)
 echo '($foo + $baz)' # ($foo + $baz)
 
-bar="I'm bar"
+# ไม่สามารถเรียก ใช้งาน variable ที่เคย execute ก่อนหน้าได้
+export bar="I'm bar"
 ./test.bash
+echo $aff
+
+# สามารถเรียก vairable ที่อยู่ใน test2.bash มาใช้งานได้ และจะหายไป
+# เมื่อ script หลักที่เรียกใช้ทำงานเสร็จ
+source test2.bash
+echo $omg
 
 # echo $bar
