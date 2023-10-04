d853e5b682fbeeeaeb381203a2daada3a3232704
Bawornsit.C <puksigon@gmail.com>
2023-10-04 11:42:55 +0700

    update: args build

--- 5_expression/args_build.bash
+++ 5_expression/args_build.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-GL_PIPELINE_BUILD_ARGS="ok"
+ENV_ALIAS=dev
+GL_PIPELINE_BUILD_ARGS="MSG=ok"
+
+if [ -z $ENV_ALIAS ]; then
+    ENV_ALIAS=prd
+fi
+BUILD_ARG="--build-arg=\"BUILD_ENV=${ENV_ALIAS}\""
 
 if [[ ! -z $GL_PIPELINE_BUILD_ARGS ]] then
-    BUILD_ARG="--build-arg=${GL_PIPELINE_BUILD_ARGS}"
+    BUILD_ARG="$BUILD_ARG --build-arg=\"${GL_PIPELINE_BUILD_ARGS}\""
 fi
 
 echo $BUILD_ARG
